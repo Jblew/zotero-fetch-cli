0800800efa31d79be2cd2e29f4b67a4812f2be4a
Jędrzej Lewandowski <jedrzejblew@gmail.com>
2021-11-19 19:34:01 +0100

    feat: fetching bibliography to file

---
+++ src/fetch-bibliography.ts
@@ -0,0 +1,24 @@
+import { getArrayFromZotero, getStringFromZotero, writeToFile } from "./utils";
+
+export type Format = "json" | "bibtex";
+
+export async function fetchBibliographyToFile({
+  destination,
+  format,
+}: {
+  destination: string;
+  format: Format;
+}) {
+  if (format === "json") {
+    const items = await getArrayFromZotero("items?format=json&limit=50");
+    console.log(items);
+    await writeToFile(destination, JSON.stringify(items, undefined, 2));
+  } else if (format === "bibtex") {
+    const bibtex = await getStringFromZotero("items?format=bibtex&limit=50");
+    console.log(bibtex);
+    await writeToFile(destination, bibtex);
+  } else {
+    throw new Error(`Unknown format ${format}`);
+  }
+  console.log("Done");
+}
--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,24 @@
 #!/usr/bin/env node
 
 import { Command } from "commander";
+import { fetchBibliographyToFile } from "./fetch-bibliography";
 const program = new Command();
 
 program
   .command("fetch-bibliography <destination>")
   .requiredOption("--format <json|bibtex>", "json|bibtex")
   .description("Fetches entire user bibliography to json or bibtex")
-  .action((destination, options) => {
+  .action(async (destination, options) => {
     console.log({
       destination,
       options,
     });
+    try {
+      await fetchBibliographyToFile({ destination, format: options.format });
+    } catch (err) {
+      console.error(err);
+      process.exit(1);
+    }
   });
 
 program.parse(process.argv);
